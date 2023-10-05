0b858223ba078b02e4d5a0a19145ecb60927a19f
Nasserb2 <Abdennasser.bedroune@gmail.com>
2023-10-04 17:05:07 -0700

    loose

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -23,6 +23,8 @@ char *create_array(unsigned int size, char c)
 			for (i = 0; i < size; i++)
 				array[i] = c;
 		}
-	}
+ar *create_array(unsigned int size, char c)
+{
+	char *array = NULL;}
 	return (array);
 }
