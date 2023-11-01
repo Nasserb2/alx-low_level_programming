d681b93e57bc71c760e1409c40f038a90e0dedce
Nasserb <Abdennasser.bedroune@gmail.com>
2023-11-01 09:13:47 -0700

    tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,29 +1,42 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
+ * str_concat - This program concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: pointer of an array of chars
  */
-
-int _strlen(char *string)
+char *str_concat(char *s1, char *s2)
 {
-	int i;
+	char *strout;
+	unsigned int i, j, k, limit;
 
-	for (i = 0; string[i] != '\0'; i++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-	return (i);
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
-
-/**
- * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
- * @s1: string 1
- * @s2: string 2
- * @n: n bytes to concat from string 2
- * Return: pointer to concatenated string
- */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *ptr;
